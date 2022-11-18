79e70c2a6621fbed736f42f38b25c1c7ced801ac
sadig05 <Sadiq.letifli70@gmail.com>
2022-11-18 11:08:51 +0400

    useRef type changed

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,7 +2,8 @@ import React, {MutableRefObject, useRef, useState} from "react";
 
 interface ITask {
     id: number,
-    task: string
+    task: string,
+    done: false,
 }
 
 export default function ToDo() {
@@ -10,23 +11,30 @@ export default function ToDo() {
     // const [task, setTask] = useState<ITask>({id: 0, task: ""});
     const [list, setList] = useState<ITask[]>([]);
 
-    const inputRef = useRef() as MutableRefObject<HTMLInputElement>;
+    const inputRef = useRef<HTMLInputElement>(null);
 
     function removeTask(id: number): void {
         setList(prev => prev.filter((task: ITask) => task.id !== id))
     }
 
     function addList(newTask: ITask): void {
-        newTask.task.trim() ? setList(prev => [...prev, newTask]) : "" ;
-         inputRef.current.value = "";
+        newTask.task.trim() ? setList(prev => [...prev, newTask]) : "";
+        if (inputRef.current !== null) {
+            inputRef.current.value = ""
+        }
     }
 
 
 
     return (
         <div>
-            <input type="text" ref={inputRef}  />
-            <button type="submit" onClick={() => addList({id: Math.floor(Math.random() * 100), task: inputRef.current.value})}>add</button>
+            <input type="text" ref={inputRef}/>
+            <button type="submit" onClick={() => addList({
+                id: Math.floor(Math.random() * 100),
+                task: inputRef.current?.value ?? "",
+                done: false
+            })}>add
+            </button>
 
             <ul>
                 {
